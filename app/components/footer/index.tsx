0ac61556ae240c3cb3d633427c0254c6ca2ff81b
Victor Ezekiel <pianistpro8@gmail.com>
2024-08-27 10:28:39 +0100

    fixed error react/no-escaped-entities

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <section className="flex-1 flex-col flex gap-5">
                 <Logo />
                 <p className="mt-3 text-gray-300 hover:text-white transition-all">
-                    So, what do you think? Ready to take the next step in your journey and bring your ideas to life? Whether it&apos;s solving complex problems, creating innovative user experiences, or simply turning your passion into reality, the opportunity is right in front of you. If you're looking for someone who&apos;s driven, skilled, and ready to make an impact, let&apos;s connect and make things happen. Together, we can build something remarkable.
+                    So, what do you think? Ready to take the next step in your journey and bring your ideas to life? Whether it&apos;s solving complex problems, creating innovative user experiences, or simply turning your passion into reality, the opportunity is right in front of you. If you&apos;re looking for someone who&apos;s driven, skilled, and ready to make an impact, let&apos;s connect and make things happen. Together, we can build something remarkable.
                 </p>
                 <form className='flex flex-col md:flex-row w-full gap-2 md:gap-0'>
                     <input className='bg-white p-3 md:w-[300px] outline-none text-black' type="text" placeholder='Email address' />
